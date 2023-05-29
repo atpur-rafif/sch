db2cb125f142a1aa8a0eb1251f64ab93097a5fe9
Muhammad Atpur Rafif <muhammadatpurrafif@gmail.com>
2023-05-29 16:10:08 +0700

    Removed old validator

--- validator.ts
+++ validator.ts
@@ -1,34 +1,104 @@
-type Schema = {
-    type: "Boolean"
-} | {
-    type: "Number"
-} | {
-    type: "String"
-} | {
-    type: "Array"
-    member: Schema
-} | {
-    type: "Object"
-    member: Record<any, Schema>
-}
+namespace Schema{
+    type GetUnion<T, U> = T extends U ? T : never;
 
-type Type<T extends Schema> = 
-    T extends { type: "Boolean" } ? boolean : 
-    T extends { type: "Number" } ? number :
-    T extends { type: "String" } ? string :
-    T extends { type: "Array", member: infer R extends Schema } ? Type<R>[] : 
-    T extends { type: "Object", member: infer R extends object } ? { -readonly [K in keyof R]: R[K] extends Schema ? Type<R[K]> : never } :
-    never
+    type OmitProp<T, U extends keyof T> = {
+        [K in keyof T as (K extends U ? never : K)]: T[K]
+    }
 
-const schema = {
-    type: "Object",
-    member: {
-        a: { type: "Number" },
-        b: {
-            type: "Array",
-            member: { type: "String" }
+    type ChangePropName<O, F extends string, T extends string> = {
+        [K in keyof O as (K extends F ? T : K)]: O[K]
+    }
+
+    type CompressParam<T> = T extends { "_params": infer P } ? (OmitProp<T, "_params"> & P) : T
+    type ExtractParamFromSchema<T extends Schema> = OmitProp<T, "type">
+    type GetParamType<T extends Config["_from"]> = GetUnion<Config, { _from: T }> extends { _params: infer R } ? R : never 
+    type GetParam<T extends Config["_from"], P> = P extends GetParamType<T> ? P : never
+
+    type UnwrapTuple<T extends ReadonlyArray<Schema>> = {
+        -readonly [K in keyof T]: Type<T[K]> extends infer K ? K : never
+    }
+
+    export type Config<P = any> = {
+        _from: "Any"
+        _to: any
+    } | {
+        _from: "Strict"
+        _to: GetParam<"Strict", P>["value"]
+        _params: {
+            value: any
+        }
+    } | {
+        _from: "Boolean"
+        _to: boolean
+    } | {
+        _from: "Number"
+        _to: number
+    } | {
+        _from: "String"
+        _to: string
+    } | {
+        _from: "Array"
+        _to: Type<GetParam<"Array", P>["element"]>[]
+        _params: {
+            element: Schema
+        }
+    } | {
+        _from: "Object"
+        _to: P extends GetParamType<"Object"> ? 
+            (P["optionalProperties"] extends object ? Partial<
+                { -readonly [K in keyof (OmitProp<P["optionalProperties"], keyof P["properties"]>)]: Type<P["optionalProperties"][K]>}
+            > : {}) &
+            { -readonly [K in keyof P["properties"]]: Type<P["properties"][K]> }
+            : never
+        _params: {
+            properties: Record<string | number, Schema>
+            optionalProperties?: Record<string | number, Schema>
+        }
+    } | {
+        _from: "Tuple"
+        _to: UnwrapTuple<GetParam<"Tuple", P>["element"]>
+        _params: {
+            element: readonly Schema[]
+        }
+    } | {
+        _from: "Union"
+        _to: UnwrapTuple<GetParam<"Union", P>["member"]>[number]
+        _params: {
+            member: readonly Schema[]
         }
     }
-} as const satisfies Schema
 
-type schemaType = Type<typeof schema>
+    // Create schema from config
+    export type Schema<C extends Config = Config> = CompressParam<ChangePropName<OmitProp<C, "_to">, "_from", "type">>
+
+    // Create type from schema
+    export type Type<T extends Schema> = T extends { type: infer R } ? GetUnion<Config<ExtractParamFromSchema<T>>, { _from: R }>["_to"] : never
+}
+
+const a = {
+    type: "Tuple",
+    element: [
+        { type: "String" },
+        {
+            type: "Tuple",
+            element: [
+                { type: "Number" },
+                {
+                    type: "Union",
+                    member: [
+                        { type: "String" },
+                        { type: "Number" }
+                    ]
+                },
+                {
+                    type: "Strict",
+                    value: [1,2,3]
+                }
+            ]
+        }
+    ]
+} as const satisfies Schema.Schema
+
+type ta = Schema.Type<typeof a>
+
+
